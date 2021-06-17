afcb2671a055b2e0bd0adb5ff09be717cd743238
anildalar <dollorinfotech@gmail.com>
2021-06-17 17:44:26 +0530

    video13 - Cart Model Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMobileAlt, faShoppingCart, faUser,faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faMobileAlt, faShoppingCart, faUser, faSearch, faChartLine,faShieldAlt } from '@fortawesome/free-solid-svg-icons'
 
 /**
 * @author
@@ -19,30 +19,77 @@ import PropTypes from 'prop-types'
 class App extends Component {
   //1. Properties
   state = {
-    x:''
+    x: ''
   }
   //2. Constructor
 
   //3. Method
-  handleScroll = ()=>{
-    console.log('ok '+window.scrollY);
-      if(window.scrollY >= 50 ){
-        //Add
-        //position-fixed top-0 start-0 
-        this.setState({x:'position-fixed top-0 start-0'});
-      }else{
-        //Remove
-        this.setState({x:''});
-      }
+  handleScroll = () => {
+    console.log('ok ' + window.scrollY);
+    if (window.scrollY >= 50) {
+      //Add
+      //position-fixed top-0 start-0 
+      this.setState({ x: 'position-fixed top-0 start-0' });
+    } else {
+      //Remove
+      this.setState({ x: '' });
+    }
   }
-  componentDidMount(){
+  componentDidMount() {
     //THis method method will be called when page/component is loaded
     //object.method();
-    window.addEventListener('scroll',this.handleScroll);
+    window.addEventListener('scroll', this.handleScroll);
   }
   render() {
     return (
       <div>
+        <div className="modal cartModel" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-xl" style={{ 'top':76+'px' }}>
+            <div className="modal-content">
+              <div className="text-center position-relative">
+                <h5 className="modal-title p-3 mt-3 text-secondary" id="exampleModalLabel">Shopping Cart is empty!</h5>
+                <h5 className="modal-title p-3 text-secondary" id="exampleModalLabel22">BROWSE CATEGORIES</h5>
+                <button type="button" className="btn-close position-absolute" style={{'right':30+'px','top':30+'px'}} data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <ul class="list-unstyled">
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                  <li><a href="#">Appliances</a></li>
+                </ul>
+              </div>
+              <div className="modal-footer d-flex justify-content-center">
+                <button type="button" className="btn btn-danger btn-lg ps-5 pe-5">Start Shopping Now</button>
+                
+              </div>
+              <p className="text-center">
+                  <span className="me-3"><FontAwesomeIcon icon={faShieldAlt}  /> Safe and Secure Payments </span>
+                  <span><FontAwesomeIcon icon={faShieldAlt}  /> 100% Payment Protection, Easy Returns Policy</span>
+                </p>
+            </div>
+          </div>
+        </div>
         <header>
           <div className="a_header_top">
             <div className="container p-0">
@@ -63,7 +110,7 @@ class App extends Component {
               </ul>
             </div>
           </div>
-          <div className={'a_header_bottom w-100 ' +this.state.x}>
+          <div className={'a_header_bottom w-100 ' + this.state.x}>
             <div className="container">
               <div className="row">
                 <div className="col-2">
@@ -74,15 +121,43 @@ class App extends Component {
                 <div className="col-7">
                   <form className="mt-3 a_hform">
                     <div className="row m-0">
-                      <div className="col-md-10 p-0">
+                      <div className="col-md-10 p-0 position-relative">
                         <input placeholder="Search products &amp; brands" className="ps-5 h-100 rounded-0 form-control" />
-                        <div className="bg-white inputBox position-absolute "></div>
+                        <div className="bg-white inputBox w-100 position-absolute">
+                          <h6 className="ps-3 mt-3"><FontAwesomeIcon icon={faChartLine} /> Trending Searches</h6>
+                          <ul className="list-unstyled">
+                            <li className="nav-item">
+                              <a className="nav-link" href="#">Shoes For Girls</a>
+                            </li>
+                            <li className="nav-item">
+                              <a className="nav-link" href="#">Baby Girls Froks</a>
+                            </li>
+                            <li className="nav-item">
+                              <a className="nav-link" href="#">Boy Shoes</a>
+                            </li>
+                            <li className="nav-item">
+                              <a className="nav-link" href="#">Saree below 1000</a>
+                            </li>
+                            <li className="nav-item">
+                              <a className="nav-link" href="#">Plazzo pant</a>
+                            </li>
+                            <li className="nav-item">
+                              <a className="nav-link" href="#">Silk Saree</a>
+                            </li>
+                            <li className="nav-item">
+                              <a className="nav-link" href="#">One Piece Dress</a>
+                            </li>
+                            <li className="nav-item">
+                              <a className="nav-link" href="#">Full TShirt Men</a>
+                            </li>
+                          </ul>
+                        </div>
                       </div>
                       <div className="col-md-2 p-0">
                         <div class="d-grid">
                           <button class="btn btn-dark rounded-0" type="submit"><FontAwesomeIcon icon={faSearch} /> Search</button>
                         </div>
-                       
+
                       </div>
                     </div>
                   </form>
@@ -90,7 +165,7 @@ class App extends Component {
                 <div className="col-3">
                   <ul className="nav a_hbot_nav float-end">
                     <li className="nav-item border-start border-end">
-                      <a className="nav-link text-white" aria-current="page" href="#">Cart <FontAwesomeIcon className="fs-5" icon={faShoppingCart} /></a>
+                      <a href="#" className="nav-link text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">Cart <FontAwesomeIcon className="fs-5" icon={faShoppingCart} /></a>
                     </li>
                     <li className="nav-item">
                       <a className="pt-3 pb-3 text-white a_bl rounded-top" href="#">Sign in
