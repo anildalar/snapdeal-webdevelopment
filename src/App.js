c82ccb03c87a1c2955afa06e1e9600891503cc81
anildalar <dollorinfotech@gmail.com>
2021-06-21 09:55:13 +0530

    video16 - Footer Heart Animation and Breadcrumb WIP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMobileAlt, faShoppingCart, faUser, faSearch, faChartLine, faShieldAlt,faBars } from '@fortawesome/free-solid-svg-icons'
+import { faMobileAlt, faShoppingCart, faUser, faSearch, faChartLine, faShieldAlt, faBars, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 /**
 * @author
@@ -20,8 +20,8 @@ class App extends Component {
   //1. Properties
   state = {
     x: '',
-    y:'',
-    z:'d-none'
+    y: '',
+    z: 'd-none'
   }
   //2. Constructor
 
@@ -36,15 +36,15 @@ class App extends Component {
       //Remove
       this.setState({ x: '' });
     }
-    if(window.scrollY >= 787){
+    if (window.scrollY >= 787) {
       this.setState({ y: 'position-fixed aside d-none' });
       this.setState({ z: 'd-block' });
-    }else{
+    } else {
       this.setState({ z: 'd-none' });
 
     }
   }
-  asideNavShow= ()=>{
+  asideNavShow = () => {
     this.setState({ y: 'position-fixed aside d-block' });
   }
   componentDidMount() {
@@ -129,7 +129,7 @@ class App extends Component {
                   <a href="#" className="btn mt-2 p-0 float-start">
                     <img src="./logo.png" />
                   </a>
-                  <a href="#" onMouseMove={this.asideNavShow} className={'mt-3 btn btn-sm btn-light float-end '+this.state.z}> <FontAwesomeIcon icon={faBars} /> </a>
+                  <a href="#" onMouseMove={this.asideNavShow} className={'mt-3 btn btn-sm btn-light float-end ' + this.state.z}> <FontAwesomeIcon icon={faBars} /> </a>
                 </div>
                 <div className="col-7">
                   <form className="mt-3 a_hform">
@@ -208,7 +208,7 @@ class App extends Component {
         <div className="a_main pb-5">
           <div className="container p-0 a_main1 mt-3">
             <div className="row m-0 a_main1_b1 ">
-              <div className={'bg-white col-2 pe-0 pt-3 pb-3 '+this.state.y}>
+              <div className={'bg-white col-2 pe-0 pt-3 pb-3 ' + this.state.y}>
                 <h6 className="fs-6 fw-light">TOP CATEGORIES</h6>
                 <ul className=" nav flex-column asidenav">
                   <li className="nav-item">
@@ -323,9 +323,131 @@ class App extends Component {
           <div className="container-fluid a_main2"></div>
         </div>
         <footer>
-          <div className="container a_foot1"></div>
-          <div className="container a_foot2"></div>
-          <div className="container a_foot3"></div>
+          <div className="container a_foot1">
+            
+          </div>
+          <div className="container a_foot2">
+          <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb mb-0">
+                <li className=""><a href="#">Mobile Accessories: </a></li>
+                <li className="ms-2 breadcrumb-item"><a href="#">Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+                <li className="breadcrumb-item" aria-current="page"><a href="#">Leather Mobile Covers</a></li>
+              </ol>
+            </nav>
+            <h2>ABOUT SNAPDEAL</h2>
+            <p>About Snapdeal – India’s Ultimate Online Shopping Site Snapdeal’s vision is to create India’s most reliable and frictionless commerce ecosystem that creates life-changing experiences for buyers and sellers. In February 2010, Kunal Bahl along with Rohit Bansal, started Snapdeal.com - India’s largest online shopping marketplace, with the widest assortment of 35 million plus products across 800 plus diverse categories from over 125,000 regional, national, and international brands and retailers. With millions of users and more than 300,000 sellers, Snapdeal is the online shopping site for Internet users across the country, delivering to 6000+ cities and towns in India. In its journey till now, Snapdeal has partnered with several global marquee investors and individuals such as SoftBank, BlackRock, Temasek, Foxconn, Alibaba, eBay Inc., Premji Invest, Intel Capital, Bessemer Venture Partners, Mr. Ratan Tata, among others. Online Shopping – A Boon The trend of online shopping is becoming a household name and so is Snapdeal. Snapdeal is the preferred choice of hundreds of thousands of online shoppers given its mammoth assortment of 15 million+ products, quick delivery even to the remotest corners of the country, and daily deals, discounts & offers to make products available at slashed down prices to our valuable customers. Get Started! Shop Online Today at Snapdeal If you have been missing out on all the fun of online shopping thinking it requires one to be a technology aficionado then we have good news for you. Shopping online particularly at Snapdeal is a child’s play; all you need is a mobile phone or laptop or tablet with Internet connection to get started. Simply log into Snapdeal.com and browse through the wide assortment of products across categories. Once you have zeroed in on your favorite products, simply place the order by filling in the details; the products will be delivered right at your doorstep. Fulfill Your Entrepreneurial Dreams! Sell Today at Snapdeal Thanks to easy-to-understand, flexible policies and SD Advisors to help sellers at each step, anyone from a manufacturer to wholesaler to retailer can sell on Snapdeal. Begin your entrepreneurial journey with Snapdeal as a seller by filling a simple registration form here. Once the registration process is done, you can start selling your products to the entire country by sitting at your home or office. Doesn't it sound thrilling? Of course, it is and the excitement will build up with every order you receive! Start selling at Snapdeal today and see your business reach staggering heights. Shop on the Go – Install Snapdeal App Today! You can shop for your favourite products at Snapdeal even on the go using Snapdeal App. Available for both Android and Apple users, the app can be downloaded from Google Play Store and Apple App Store respectively. The app is quick, user-friendly, and enables shoppers buy products with a breeze. What's more, get timely notifications on your phone or tablet so that you don't miss amazing deals and offers. Download the app right now and experience how fun it is to shop on your mobile!</p>
+          </div>
+          <div className="container-fluid bg-white p-0 a_foot3">
+            <div className="row m-0 h-100">
+              <div className="col-10 p-0 pt-2 tbdr">
+                <span>Copyright © 2020, Snapdeal Private Limited (formerly Jasper Infotech Private Limited). All Rights Reserved</span>
+              </div>
+              <div className="col-2 p-0 pt-2  text-end tbdr ">Made with <FontAwesomeIcon className="text-danger a_heart" icon={faHeart} /> in india</div>
+            </div>
+          </div>
         </footer>
       </div>
     )
